84a34df2a34d569fce44778abaca82641d48c278
Polo Charby <polo_asm@hotmail.fr>
2024-12-23 00:05:32 +0100

    tests for flat output

--- tests/enum_test.rs
+++ tests/enum_test.rs
@@ -18,6 +18,22 @@ fn standard_test(source_name: &str) {
     compare_folders_content(dest_folder.as_str(), expected_folder.as_str());
 }
 
+fn standard_onefile_test(source_name: &str) {
+    let source_file = format!("test_schemas/{}.avsc", source_name);
+    let dest_folder = format!("target/tmp/.result/{}/", source_name);
+    let expected_folder = format!("test_schemas/{}/expected1/", source_name);
+
+    Avrogen::new()
+        .add_source(source_file.as_str())
+        .output_folder_from_str(dest_folder.as_str())
+        .set_flat_output()
+        .set_verbosity_debug()
+        .execute()
+        .expect("No error should appear");
+
+    compare_folders_content(dest_folder.as_str(), expected_folder.as_str());
+}
+
 #[test]
 fn convert_simple_enum() {
     standard_test("simple_enum");
@@ -31,4 +47,9 @@ fn convert_simple_record() {
 #[test]
 fn convert_recursive_record() {
     standard_test("recursive_record");
+}
+
+#[test]
+fn convert_recursive_record_onefile() {
+    standard_onefile_test("recursive_record");
 }
